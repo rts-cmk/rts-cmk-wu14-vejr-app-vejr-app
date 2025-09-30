ef24fca0453f3817a2d78afb04b38025a1543e32
Gingersmurlf <theodorenghoff@gmail.com>
2025-09-30 14:15:03 +0200

    nu har vi næsten lavet alt

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,48 @@
 import { useState } from "react";
 import "./css/main.css";
 
+import SearchInput from "./components/SearchInput";
+import WeatherInfo from "./components/WeatherInfo";
+import { API_KEY } from "./components/apiKey"
+
 export default function App() {
   const [by, setBy] = useState(null);
-  const [lat, setLat] = useState(null);
-  const [lon, setLon] = useState(null);
   const [temperatur, setTemperatur] = useState(null);
+  const [følesSom, setFølesSom] = useState(null);
+  
 
-  async function findBy() {
-    const cityInput = document.getElementById("cityInput").value;
-
+  async function findBy(cityInput) {
     setBy(cityInput);
 
-    const cityFinder = `http://api.openweathermap.org/geo/1.0/direct?q=${by}&limit=5&appid=d7dbd4f34253c695f3efc75396c59fd4`;
-    fetch(cityFinder)
-      .then((svar) => svar.json())
-      .then((data) => {
-        setLat(data[0].lat);
-        setLon(data[0].lon);
-      });
-  }
-  console.log(lat);
-  console.log(lon);
+    const cityFinder = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=2&appid=${API_KEY}`;
+    const res1 = await fetch(cityFinder);
+    const data1 = await res1.json();
 
-  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d7dbd4f34253c695f3efc75396c59fd4&units=metric`;
-  fetch(weatherURL)
-    .then((svar) => svar.json())
-    .then((data) => {
-      console.log(data);
-      setTemperatur(data.main.temp.toFixed(1));
-    });
+    const lat = data1[0].lat.toString();
+    const lon = data1[0].lon.toString();
+
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const res2 = await fetch(weatherUrl);
+    const data2 = await res2.json();
+
+    setTemperatur(data2.main.temp.toFixed(1) + "°");
+    setFølesSom(data2.main.feels_like.toFixed(1) + "°");
+
+    const futureWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const res3 = await fetch(futureWeather)
+    const data3 = await res3.json()
+    data3.list.map(data67 => {
+      console.log(data67);
+      
+    })
+    
+  }
 
   return (
     <main className="weather-main">
-      <div>
-        <input
-          onKeyDown={(e) => {
-            if (e.key === "Enter") findBy();
-          }}
-          className="city-input"
-          type="text"
-          name=""
-          id="cityInput"
-          placeholder="Indsæt by her:"
-        />
-      </div>
-      <div>
-        <div>
-          <h3 className="city-name">Temperatur i {by}</h3>
-          <h2 className="city-temp">{temperatur}</h2>
-          <p></p>
-        </div>
+      <SearchInput onSearch={findBy} />
+      <div className="weather-container">
+        <WeatherInfo by={by} temperatur={temperatur} følesSom={følesSom} />
       </div>
     </main>
   );
